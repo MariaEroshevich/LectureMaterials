f4701194e433c4270d70e1ae924e4da66d45b514
Liliya Peravozchykava <lily.brightlight@mail.ru>
2022-10-30 19:56:11 +0300

    preparation code

---
+++ LINQ/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualBasic;
+
+var names = new List<string>();
+
+var queryExtension = names.Where(n => n.Contains('f')).Select(n => n.ToUpper()).OrderBy(n => n.Length);
+var queryQuery = from n in names
+                 where n.Contains('f')
+                 orderby n.Length
+                 select n.ToUpper();
+
+var objects = new List<object> { "sdfsf", 32, 85.4 };
+var query = objects.OfType<string>().ToList();
+Console.ReadLine();
